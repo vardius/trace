2b6005e5418be47f48709e0a66f90d4c588b96e0
Rafał Lorenz <vardius@gmail.com>
2019-06-26 08:49:51 +1000

    Update example_test.go

--- example_test.go
+++ example_test.go
@@ -9,7 +9,7 @@ import (
 
 func Example_here() {
 	c := func() {
-		fmt.Printf("%s\n%s", "Hello from:", trace.Here(Lfile | Lline | Lfunction))
+		fmt.Printf("%s\n%s", "Hello from:", trace.Here(trace.Lfile | trace.Lline | trace.Lfunction))
 	}
 
 	b := func() { c() }
@@ -25,7 +25,7 @@ func Example_here() {
 func Example_here_second() {
 	err := errors.New("Internal system error")
 
-	fmt.Printf("%s %s\n%s", "Error:", err, trace.Here(Lfile | Lline | Lfunction))
+	fmt.Printf("%s %s\n%s", "Error:", err, trace.Here(trace.Lfile | trace.Lline | trace.Lfunction))
 
 	// Output:
 	// Error: Internal system error
