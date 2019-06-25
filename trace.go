a174cfc8177013a5e0d53e09b2e100c01efce3da
Rafał Lorenz <vardius@gmail.com>
2019-06-26 08:45:43 +1000

    Update trace.go

--- trace.go
+++ trace.go
@@ -6,38 +6,56 @@ import (
 	"runtime"
 )
 
+const (
+	Lfile      = 1 << iota             // full file name
+	Lline                              // line number
+	Lfunction                          // name of the function
+	LstdFlags  = Llongfile | Lfunction // initial values
+)
+
 // Here returns string representation of a reference
-// to a File:Line Function calling this function
-func Here() string {
-	var buf bytes.Buffer
-
-	// Ask for up to 3 pcs, including:
-	// - runtime.Callers,
-	// - trace.Here
-	// - pcs for where golog.getPCs was called
-	pc := make([]uintptr, 3)
-	n := runtime.Callers(0, pc)
-
-	if n < 3 {
-		pc = pc[:]
+// default flags LstdFlags
+func Here(flag int) string {
+	if flag == 0 {
+		flag = LstdFlags
 	}
 
-	// pass only valid pcs to runtime.CallersFrames
-	// exclude irrelevant pcs
-	pc = pc[2:n]
+	var buf bytes.Buffer
 
-	if len(pc) > 0 {
-		frames := runtime.CallersFrames(pc)
-		// Loop to get frames.
-		// A fixed number of pcs can expand to an indefinite number of Frames.
-		for {
-			frame, more := frames.Next()
-			fmt.Fprintf(&buf, "%s:%d %s", frame.File, frame.Line, frame.Function)
-			if !more {
-				break
-			}
+	frame := getFrame(3)
+	if frame != nil {
+		if flag&Lfile != 0 {
+			fmt.Fprintf(&buf, "%s", frame.File)
+		}
+		if flag&Lline != 0 {
+			fmt.Fprintf(&buf, ":%d", frame.Line)
+		}
+		if flag&Lfunction != 0 {
+			fmt.Fprintf(&buf, " %s", frame.Function)
 		}
 	}
 
 	return buf.String()
+}
+
+func getFrame(calldepth int) *runtime.Frame {
+	pc, file, line, ok := runtime.Caller(calldepth)
+	if !ok {
+		return nil
+	}
+
+	frame := &runtime.Frame{
+		PC:   pc,
+		File: file,
+		Line: line,
+	}
+
+	funcForPc := runtime.FuncForPC(pc)
+	if funcForPc != nil {
+		frame.Func = funcForPc
+		frame.Function = funcForPc.Name()
+		frame.Entry = funcForPc.Entry()
+	}
+
+	return frame
 }
